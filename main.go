9fd7c4105604ab5d2c595fac7093a02aa1b6087e
ksabanty <kgsabanty@gmail.com>
2025-06-18 20:21:02 -0400

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "html/template"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("templates/index.html"))
+        tmpl.Execute(w, nil)
+    })
+    http.ListenAndServe(":8080", nil)
+}
+
